3302ae050056d25acf1d527073b9a6ea160f8371
Ojurek <ojurek@wp.pl>
2021-01-11 16:45:39 +0100

    find finished

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -64,7 +64,7 @@ class BinarySearchTree: public Tree<T>{
             return getMinValue_help(checked_node->left);
         }
 
-        bool find_help(const T &data, std::unique_ptr<Node> &checked_node) const{
+        bool find_help(const T &data, const std::unique_ptr<Node> &checked_node) const{
             
             if (checked_node->m_data > data){
                 if (!checked_node->left) {
@@ -123,24 +123,8 @@ class BinarySearchTree: public Tree<T>{
             if (!root){
                 return false;
             }
-            /*
-            if (checked_node->m_data > data){
-                if (!checked_node->left) {
-                    return false;
-                } 
-                return find_help(data, checked_node->left);
-            }
 
-            if (checked_node->m_data < data){
-                if (!checked_node->right) {
-                    return false;
-                } 
-                return find_help(data, checked_node->right);
-            }
-            return true;
-            */
             return find_help(data, root);
-               
         }
 
 };
--- main.cpp
+++ main.cpp
@@ -16,8 +16,8 @@ int main(int argc, char const *argv[])
     std::cout<<"Tree max value = "<<tree.getMaxValue()<<std::endl;    
     std::cout<<"Tree min value = "<<tree.getMinValue()<<std::endl;   
 
-    std::cout<<"Find 5 = "<<tree.find(5)<<std::endl;
-    std::cout<<"Find 100 = "<<tree.find(100)<<std::endl;
+    std::cout<<"Find 5 = "<<std::boolalpha<<tree.find(5)<<std::endl;
+    std::cout<<"Find 100 = "<<std::boolalpha<<tree.find(100)<<std::endl;
 
 
     return 0;
